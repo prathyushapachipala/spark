020bb126f2a4ccf769756fa07890c9f74aa55137
prathyushapachipala <49972745+prathyushapachipala@users.noreply.github.com>
2019-06-07 20:09:33 +0530

    Add files via upload

---
+++ update.php
@@ -0,0 +1,34 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: PUT');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Credit.php';
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $credit = new Credit($db);
+
+  // Get raw posted data
+  $data = json_decode(file_get_contents("php://input"));
+
+  // Set ID to UPDATE
+  $credit->credit_id = $data->credit_id;
+
+  $credit->username = $data->username;
+
+  // Update post
+  if($credit->update()) {
+    echo json_encode(
+      array('message' => 'Credit Updated')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Credit not updated')
+    );
+  }
